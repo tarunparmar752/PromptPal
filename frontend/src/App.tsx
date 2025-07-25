32f7b753baca88ccf479b839a6d0b5df2face3cc
Tarun Parmar <tarunparmar752@gmail.com>
2025-07-25 08:41:52 +0530

    feat: Implement Layout component with Navbar and Outlet for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import Layout from "./Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 }
